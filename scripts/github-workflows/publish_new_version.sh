b465450eb07263cc965ec4b59bb2801a47c25580
Jackson Dean <jckson.dean@gmail.com>
2023-04-26 12:23:56 -0700

    update publish script

--- scripts/github-workflows/publish_new_version.sh
+++ scripts/github-workflows/publish_new_version.sh
@@ -5,26 +5,22 @@ set -e
 # Script used in github actions workflows to publish based on release tag.
 
 # Tags should be formatted in the following way:
-# <package-name>/<version-number>
-# example: deso-protocol/0.0.1
+# v<version-number>
+# example: v0.0.1
 #
 # For a pre-release, use the following format:
-# <package-name>/<version-number>-beta.<pre-release-version>
-# example: deso-protocol/0.0.1-beta.0
-LAST_TAG=$(git describe --tags --abbrev=0)
-PACKAGE=$(echo $LAST_TAG | cut -d/ -f1)
-NEW_VERSION=$(echo $LAST_TAG | cut -d/ -f2)
+# <version-number>-beta.<pre-release-version>
+# example: 0.0.1-beta.0
+NEW_VERSION=$(git describe --tags --abbrev=0)
 NPM_PRERELEASE_TAG=$(echo $NEW_VERSION | cut -d '-' -f 2 | cut -d '.' -f 1)
 
-echo "Preparing to release $PACKAGE@$NEW_VERSION"
+echo "Preparing to release deso-protocol@$NEW_VERSION"
 echo "Pre-relesae tag: $NPM_PRERELEASE_TAG"
 
 npm ci
-cd libs/$PACKAGE
 npm version $NEW_VERSION
-cd -
-npx nx run $PACKAGE:build
-cd dist/libs/$PACKAGE
+npm run build
+cp {package.json,README.md} ./lib
 
 # If the version is a pre-release (beta), publish with the --tag flag.
 if [[ $NPM_PRERELEASE_TAG == beta ]]; then
@@ -35,14 +31,13 @@ elif [[ $NPM_PRERELEASE_TAG =~ ^[0-9]+$ ]]; then
   echo "Publishing latest stable version $NEW_VERSION"
   npm publish --access public
 else
-  echo "Invalid version format for $NEW_VERSION. Please use the following format: <package-name>/<version-number> or <package-name>/<version-number>-beta.<pre-release-version>"
+  echo "Invalid version format for $NEW_VERSION. Please use the following format: v<version-number> or v<version-number>-beta.<pre-release-version>"
   exit 1
 fi
 
 RELEASE_VERSION=$(grep version package.json | awk -F \" '{print $4}')
 echo "::notice::New version successfully released: $RELEASE_VERSION"
-cd -
-git add libs/$PACKAGE/package.json
-git commit -m "ci: automated release version $PACKAGE@$RELEASE_VERSION"
+git add package.json
+git commit -m "ci: automated release version v$RELEASE_VERSION"
 git pull --rebase origin main
 git push origin HEAD:main
