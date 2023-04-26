4be052e9c7a9dab92fa8e441720d3ea935493b38
Jackson Dean <jckson.dean@gmail.com>
2023-04-26 14:02:01 -0700

    skip husky for npm publish

--- scripts/github-workflows/publish_new_version.sh
+++ scripts/github-workflows/publish_new_version.sh
@@ -17,7 +17,9 @@ NPM_PRERELEASE_TAG=$(echo $NEW_VERSION | cut -d '-' -f 2 | cut -d '.' -f 1)
 echo "Preparing to release $NEW_VERSION"
 echo "Pre-release tag: $NPM_PRERELEASE_TAG"
 
-npm ci
+# skip setting up husky
+npm set-script prepare ""
+npm ci --only=production
 npm version --no-git-tag-version $NEW_VERSION
 npm run build
 cp {package.json,README.md} ./lib
