a0b92e6a6edf04536705ca1ea201600b7fe73610
Jackson Dean <jckson.dean@gmail.com>
2023-05-14 16:29:05 -0700

    add more docs

--- scripts/package-react-native.sh
+++ scripts/package-react-native.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-# NOTE: we overwrite the identity-instance.ts file with the native version so
-# the correct type definitions are exported for react-native. This is the only
-# difference the web version and the react-native version.
+# NOTE: we overwrite the identity-instance.ts file with the react native version
+# so the correct type definitions are exported for react-native. This is the
+# only difference the web version and the react-native version. This is a hacky
+# way to do this but it works for now. If you know of a better way to do this,
+# please feel free to submit a PR.
 mv ./src/identity/identity-instance.react-native.ts ./src/identity/identity-instance.ts
 npm run build
 cp {package.json,LICENSE} ./lib
--- src/identity/identity-instance.ts
+++ src/identity/identity-instance.ts
@@ -1,4 +1,6 @@
 import { api } from '../data';
 import { Identity } from './identity';
 
+// NOTE: Be careful making changes here as this file will be overwritten
+// during the build for react-native. See ./scripts/package-react-native.sh
 export const identity = new Identity<Storage>(globalThis, api);
